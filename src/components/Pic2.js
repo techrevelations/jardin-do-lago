8e2db33615e107972fb2eae4ccc7b842084908e4
Admin <admin@Admins-MBP.lan>
2019-04-10 13:13:55 +0100

    again

--- src/components/Pic2.js
+++ src/components/Pic2.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 export default class Pic2 extends Component {
   render () {
     return (
+      // image 04 is preferable but unable to deploy to heroku atm
       <div className='pic2'>
-        <img id='pic2' src={'/media/img_04.jpg'} alt={'/media/img_06.jpg'} />
+        <img id='pic2' src={'/media/img_06.jpg'} alt={'/media/img_06.jpg'} />
       </div>
     )
   }
