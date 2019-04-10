fb575566229acd13640a5475bbe0f192e2c03723
Admin <admin@Admins-MBP.lan>
2019-04-10 13:07:58 +0100

    pic alteration

--- src/components/Pic2.js
+++ src/components/Pic2.js
@@ -4,7 +4,7 @@ export default class Pic2 extends Component {
   render () {
     return (
       <div className='pic2'>
-        <img id='pic2' src={'/media/img_04.jpg'} alt='jardin_do_lago' />
+        <img id='pic2' src={'/media/img_04.jpg'} alt={'/media/img_06.jpg'} />
       </div>
     )
   }
