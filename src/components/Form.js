e3fcd3523fecff3a893de4b0540a1338c0d3134a
Admin <admin@Admins-MBP.lan>
2019-04-15 22:45:35 +0100

    touch ups

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,7 +7,9 @@ export default class Form extends Component {
       <div className='form'>
         <Reveal effect='fadeInUp'>
           <img id='pic6' src={'/media/img_12.jpg'} alt='jardin_do_lago' />
-          <div className='title1'>FALE CONOSCO</div>
+          <div className='title1' id='fale'>
+            FALE CONOSCO
+          </div>
           <form className='form2'>
             <input className='telephone' type='tel' placeholder='Telefone para retorno:' />
             <input className='name' type='text' placeholder='Seu nome:' />
